ef9f2c9baea7766ad6cd9b47df0221c37ba767ca
Tollkien <AustinJBreaux@outlook.com>
2022-09-20 21:20:45 -0500

    Initial commit

---
+++ Javascript Exercises Alt.js
@@ -0,0 +1,10 @@
+const repeatString = function(Number) {
+    let repeatNumber = Number;
+    let heyArray = [""]
+    if(isInteger(repeatNumber)){
+        for(let i = 0; i < repeatNumber; i++){
+            heyArray = "hey" + heyArray;
+        }
+    	return;
+    }
+};
