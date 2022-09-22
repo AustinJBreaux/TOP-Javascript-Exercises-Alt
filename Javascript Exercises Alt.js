019cd302629358f66d06d9c62abaca85e876e828
Tollkien <AustinJBreaux@outlook.com>
2022-09-21 19:46:28 -0500

    Pass test 5 of repeatString

--- Javascript Exercises Alt.js
+++ Javascript Exercises Alt.js
@@ -1,6 +1,6 @@
 const repeatString = function(repeatNumber){
     let heyArray = "";
-    if(Number.isInteger(repeatNumber)){
+    if(Number.isInteger(repeatNumber) && repeatNumber >= 0){
         for(let i = 0; i < repeatNumber; i++){
             heyArray = "hey" + heyArray;
         }
