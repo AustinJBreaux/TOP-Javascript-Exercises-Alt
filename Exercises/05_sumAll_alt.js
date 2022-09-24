0044eaf8995fd51916a121dcf27c53b180daa0f0
Tollkien <AustinJBreaux@outlook.com>
2022-09-24 17:44:16 -0500

    Pass all tests

--- Exercises/05_sumAll_alt.js
+++ Exercises/05_sumAll_alt.js
@@ -1,15 +1,33 @@
 const sumNumbers = function sum(lower, upper){
-    let counter = lower;
-    let accumulatedValue = lower;
-    if(lower > 0 && !isNaN(lower) &&
-    upper > 0 && !isNaN(upper)){
-        do{
-            counter = counter +1;
-            accumulatedValue = accumulatedValue + counter;
+    if(lower > upper){//Switch upper and lower variables if lower > upper
+        temp = lower;
+        lower = upper;
+        upper = temp;
+        let counter = lower;
+        let accumulatedValue = lower;
+        if(lower > 0 && !isNaN(lower) &&
+        upper > 0 && !isNaN(upper)){
+            do{
+                counter = counter +1;
+                accumulatedValue = accumulatedValue + counter;
+            }
+            while(counter != upper){
+                return accumulatedValue;
+            }
         }
-        while(counter != upper){
-            return accumulatedValue;
-        }
+        else return "Error";
     }
-    else return "Error";
+    let counter = lower;
+        let accumulatedValue = lower;
+        if(lower > 0 && !isNaN(lower) &&
+        upper > 0 && !isNaN(upper)){
+            do{
+                counter = counter +1;
+                accumulatedValue = accumulatedValue + counter;
+            }
+            while(counter != upper){
+                return accumulatedValue;
+            }
+        }
+        else return "Error";
 }
