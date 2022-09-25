7c87b3ca66ec76c8e8cf48b43a063dffd621ccd8
Tollkien <AustinJBreaux@outlook.com>
2022-09-25 14:59:05 -0500

    Pass test 2

--- Exercises/09_palindromes_alt.js
+++ Exercises/09_palindromes_alt.js
@@ -1,5 +1,9 @@
 const isPalindrome = function isPalindrome(readLtoR){
+    let punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    readLtoR = readLtoR.replace(punctuation, "");
     readRtoL = readLtoR.split("").reverse().join("");
+    readRtoL = readRtoL.replace(punctuation, "");
+    console.log(readRtoL)
     if(readRtoL === readLtoR){
         return true;
     }
