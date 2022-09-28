9f5ce7443b43c29aa2384cd74e387d173f0b2a3c
Tollkien <AustinJBreaux@outlook.com>
2022-09-27 21:24:50 -0500

    Passes all tests

--- Exercises/10_fibonacci_alt.js
+++ Exercises/10_fibonacci_alt.js
@@ -1,7 +1,19 @@
 const getFibonacci = function fibonacci(n){
-    function fib(n) {
-    if (n <= 1)
-        return n;
-    return fib(n-1) + fib(n-2);
+    n = parseInt(n, 10);
+        if (n < 0){
+            return "OOPS"
+        }
+        else if (n <= 1)
+            return n;
+        else{
+            let a = 0;
+            let b = 1;
+            let c = n;
+            for(let i = 2; i <= n; i++) {
+                c = a + b;
+                 a = b;
+                 b = c;
+            }
+            return c;
     }
 }
