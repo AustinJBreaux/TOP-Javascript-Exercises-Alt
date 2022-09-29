2543db30b755e6d8be3103495e0d17be428c6973
Tollkien <AustinJBreaux@outlook.com>
2022-09-28 20:48:05 -0500

    Attempts but does not pass exercise 11

---
+++ Exercises/11_getTheTitles_alt.js
@@ -0,0 +1,14 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+let result = books.map(book => book.title);
+let getTitle = function title(result){
+    return result;
+}
