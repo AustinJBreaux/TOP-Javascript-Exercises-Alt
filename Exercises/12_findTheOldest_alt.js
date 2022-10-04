16fcca28bef87d052036f0142c4b793e3d1f9435
Tollkien <AustinJBreaux@outlook.com>
2022-10-03 21:13:28 -0500

    Passes test 1

--- Exercises/12_findTheOldest_alt.js
+++ Exercises/12_findTheOldest_alt.js
@@ -15,7 +15,25 @@ const people1 = [ //Test 1
       yearOfDeath: 1941,
     },
 ]
-
+const test1 = function test1(){
+  difference1 = people1[0].yearOfDeath - people1[0].yearOfBirth;
+  difference2 = people1[1].yearOfDeath - people1[1].yearOfBirth;
+  difference3 = people1[2].yearOfDeath - people1[2].yearOfBirth;
+  if(difference1 > difference2 && difference3){
+    return people1[0].name;
+  }
+  else if(difference2 > difference1 && difference3){
+    return people1[1].name
+  }
+  else if(difference3 > difference1 && difference20){
+    return people1[2].name;
+  }
+}
+/*Pseudo: Find oldest person
+Get difference between death and birth = death - birth
+Test difference between the three objects Math.max()
+Return name of object containing greatest difference
+*/
 const people2 = [ //Test 2
     {
       name: "Carly",
